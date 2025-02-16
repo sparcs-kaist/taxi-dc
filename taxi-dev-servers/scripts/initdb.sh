67730e3c0c036042599c28c5fbf35867ccd4ceb3
TaehyeonPark <devtaehyeon@gmail.com>
2025-02-16 19:08:10 +0900

    Add: DB initialization script

---
+++ taxi-dev-servers/scripts/initdb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ -f ".env" ]; then
+    export $(grep -v '^#' .env | xargs)
+else
+    echo "Error: .env file not found!"
+    exit 1
+fi
+
+if [ -z "$DEV_USER" ]; then
+    echo "Error: DEV_USER is not set in .env. Exiting..."
+    exit 1
+fi
+
+echo $DEV_USER
+echo $MONGO_ROOT_USERNAME
+echo $MONGO_ROOT_PASSWORD
+echo $DEFAULT_PASSWORD
+
+echo "Creating MongoDB user and database for: $DEV_USER"
+docker exec -it taxi-mongo-shared mongo -u "$MONGO_ROOT_USERNAME" -p "$MONGO_ROOT_PASSWORD" --authenticationDatabase admin --eval \
+"db.createUser({user: \"$DEV_USER\", pwd: \"$DEFAULT_PASSWORD\", roles: [{ role: \"dbOwner\", db: \"$DEV_USER\" }]});"
+echo "MongoDB user and database for $DEV_USER created successfully!"
+
