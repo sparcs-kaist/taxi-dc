8f1179c9e58cee0115f1d39a3d4599165afccdd7
static <kmc7468@naver.com>
2025-04-20 08:20:13 +0900

    Edit: stop.sh

--- taxi-dev-servers/scripts/stop.sh
+++ taxi-dev-servers/scripts/stop.sh
@@ -2,14 +2,14 @@
 
 echo -n "taxi-front: "
 if screen -ls | grep -q "\.taxi-front"; then
-        screen -S taxi-front -X quit && echo "ok" || echo "failed"
+        screen -S taxi-front -X stuff "^C" && echo "ok" || echo "failed"
 else
         echo "not running"
 fi
 
 echo -n "taxi-back: "
 if screen -ls | grep -q "\.taxi-back"; then
-        screen -S taxi-back -X quit && echo "ok" || echo "failed"
+        screen -S taxi-back -X stuff "^C" && echo "ok" || echo "failed"
 else
         echo "not running"
 fi
